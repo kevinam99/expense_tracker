2a365289833f30de04430b4246dcc0ed0ae2e8d2
Kevin <kevinam99@gmail.com>
2025-07-30 11:48:38 +0530

    Fix broken tests

--- test/expense_tracker/expenses_test.exs
+++ test/expense_tracker/expenses_test.exs
@@ -74,28 +74,31 @@ defmodule ExpenseTracker.ExpensesTest do
     @invalid_attrs %{date: nil, description: nil, purpose: nil, amount: nil, optional_notes: nil}
 
     test "list_expenses/0 returns all expenses" do
-      expense = expense_fixture()
+      category = category_fixture()
+      expense = expense_fixture(%{category_id: category.id})
       assert Expenses.list_expenses() == [expense]
     end
 
     test "get_expense!/1 returns the expense with given id" do
-      expense = expense_fixture()
+      category = category_fixture()
+      expense = expense_fixture(%{category_id: category.id})
       assert Expenses.get_expense!(expense.id) == expense
     end
 
     test "create_expense/1 with valid data creates a expense" do
+      category = category_fixture()
+
       valid_attrs = %{
         date: ~D[2025-07-29],
         description: "some description",
-        purpose: "some purpose",
         amount: "120.5",
-        optional_notes: "some optional_notes"
+        optional_notes: "some optional_notes",
+        category_id: category.id
       }
 
       assert {:ok, %Expense{} = expense} = Expenses.create_expense(valid_attrs)
       assert expense.date == ~D[2025-07-29]
       assert expense.description == "some description"
-      assert expense.purpose == "some purpose"
       assert expense.amount == Decimal.new("120.5")
       assert expense.optional_notes == "some optional_notes"
     end
@@ -105,12 +108,12 @@ defmodule ExpenseTracker.ExpensesTest do
     end
 
     test "update_expense/2 with valid data updates the expense" do
-      expense = expense_fixture()
+      category = category_fixture()
+      expense = expense_fixture(%{category_id: category.id})
 
       update_attrs = %{
         date: ~D[2025-07-30],
         description: "some updated description",
-        purpose: "some updated purpose",
         amount: "456.7",
         optional_notes: "some updated optional_notes"
       }
@@ -118,25 +121,27 @@ defmodule ExpenseTracker.ExpensesTest do
       assert {:ok, %Expense{} = expense} = Expenses.update_expense(expense, update_attrs)
       assert expense.date == ~D[2025-07-30]
       assert expense.description == "some updated description"
-      assert expense.purpose == "some updated purpose"
       assert expense.amount == Decimal.new("456.7")
       assert expense.optional_notes == "some updated optional_notes"
     end
 
     test "update_expense/2 with invalid data returns error changeset" do
-      expense = expense_fixture()
+      category = category_fixture()
+      expense = expense_fixture(%{category_id: category.id})
       assert {:error, %Ecto.Changeset{}} = Expenses.update_expense(expense, @invalid_attrs)
       assert expense == Expenses.get_expense!(expense.id)
     end
 
     test "delete_expense/1 deletes the expense" do
-      expense = expense_fixture()
+      category = category_fixture()
+      expense = expense_fixture(%{category_id: category.id})
       assert {:ok, %Expense{}} = Expenses.delete_expense(expense)
       assert_raise Ecto.NoResultsError, fn -> Expenses.get_expense!(expense.id) end
     end
 
     test "change_expense/1 returns a expense changeset" do
-      expense = expense_fixture()
+      category = category_fixture()
+      expense = expense_fixture(%{category_id: category.id})
       assert %Ecto.Changeset{} = Expenses.change_expense(expense)
     end
   end
